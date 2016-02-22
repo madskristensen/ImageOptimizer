d7382bcdd4f2e937274edb334f65f9974d7608ea
Mads Kristensen <post@madskristensen.net>
2016-02-22 13:36:06 -0800

    v3.5 [release]

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -5,7 +5,7 @@ namespace MadsKristensen.ImageOptimizer
 		public const string Id = "bf95754f-93d3-42ff-bfe3-e05d23188b08";
 		public const string Name = "Image Optimizer";
 		public const string Description = "Uses industry standard tools to optimize any JPEG, PNG and Gifs - including animated Gifs. Can do both lossy and lossless optimization.";
-		public const string Version = "3.4";
+		public const string Version = "3.5";
 		public const string Author = "Mads Kristensen";
 		public const string Tags = "image, png, gif, jpg, jpeg, compression, optimization, zopfli";
 	}
