9f7e4111213b869ab4ccee036f6963b3d9022583
Mads Kristensen <post@madskristensen.net>
2016-02-21 23:34:17 -0800

    Best png compression so far

--- src/Resources/Tools/png-lossless.cmd
+++ src/Resources/Tools/png-lossless.cmd
@@ -1,4 +1,7 @@
 ::optipng %1 -out %2 -o5 -i0
 ::pngout %1 %2 /s0 /y /kpHYs
 ::pngzopfli %1 10 %2
-zopflipng %1 %2
+truepng /o4 %1 /out %2
+zopflipng --ohh %2 %2.png
+copy %2.png %2 /y
+del %2.png
--- test/ImageOptimizer.Test/Test.cs
+++ test/ImageOptimizer.Test/Test.cs
@@ -44,7 +44,7 @@ namespace ImageOptimizer.Test
         {
             long savings = ExecuteTest("*.png", false);
 
-            Assert.IsTrue(savings == 28619, "Don't compress enough");
+            Assert.IsTrue(savings == 28855, "Don't compress enough");
         }
 
         [TestMethod, TestCategory("PNG")]
@@ -52,7 +52,7 @@ namespace ImageOptimizer.Test
         {
             long savings = ExecuteTest("*.png", true);
 
-            Assert.IsTrue(savings == 78181, "Don't compress enough");
+            Assert.IsTrue(savings == 135289, "Don't compress enough");
         }
 
         [TestMethod, TestCategory("GIF")]
@@ -100,7 +100,7 @@ namespace ImageOptimizer.Test
 
             foreach (var group in grouped)
             {
-                var sum = group.Sum(g =>  g.Saving);
+                var sum = group.Sum(g => g.Saving);
                 var time = group.Average(g => g.Elapsed.TotalSeconds);
                 total += sum;
                 sb.AppendLine(group.Key + "\t" + group.Count() + "\t" + sum + "\t" + Math.Round(time, 2));
