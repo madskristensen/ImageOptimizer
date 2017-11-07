8fa9a90c35281b58c068a436312f667f5dd1daae
Mads Kristensen <post@madskristensen.net>
2017-11-07 11:39:59 -0800

    Cleaned up old tools

--- src/Resources/Tools/png-lossless.cmd
+++ src/Resources/Tools/png-lossless.cmd
@@ -1,13 +1,2 @@
 if not exist %2 copy %1 %2 /y
 pingo -s6 -q %2
-
-exit
-pngout %1 %2 /s2 /y /kpHYs
-if not exist %2 copy %1 %2 /y
-
-zopflipng --ohh %2 %2.png
-
-if exist %2.png (
-    copy %2.png %2 /y
-    del %2.png
-)
